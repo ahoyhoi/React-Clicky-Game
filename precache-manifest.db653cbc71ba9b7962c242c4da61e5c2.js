02bd0d115e51a8a463ed3b3822a684dd1b23e117
Hoi So <miss.hoiso@gmail.com>
2019-11-02 10:40:20 -0400

    Updates

---
+++ precache-manifest.db653cbc71ba9b7962c242c4da61e5c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fed50556d6d810ebd08c3daa6b6cba",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "2f3d62967f7221237ccd",
+    "url": "/React-Clicky-Game/static/css/main.7d567377.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/React-Clicky-Game/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "2f3d62967f7221237ccd",
+    "url": "/React-Clicky-Game/static/js/main.6a2b6ed5.chunk.js"
+  },
+  {
+    "revision": "487dfcb21d3a5e9c3c3f",
+    "url": "/React-Clicky-Game/static/js/runtime-main.ccb53038.js"
+  }
+]);
